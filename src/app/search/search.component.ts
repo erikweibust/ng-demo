4b6724de0f51b41b2b63c6d8e63af04e35d8a53a
Erik Weibust <erik@weibust.net>
2021-12-17 20:39:56 -0600

    added proper search - p30

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-//import { error } from 'console';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Person, SearchService } from '../shared';
+
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
 
   query!: string;
   searchResults: Person[] = [];
+  sub!: Subscription;
 
-  constructor(private searchService: SearchService) { }
+  constructor(private searchService: SearchService, 
+    private route: ActivatedRoute) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.sub = this.route.params.subscribe( params => {
+      if (params.term) {
+        this.query = decodeURIComponent(params.term);
+        this.search();
+      }
+    });
+   }
 
   search(): void { 
     this.searchService.search(this.query).subscribe(
       (data: Person[]) => { this.searchResults = data; },
       error => console.log(error)
     );
+  }
+
+  ngOnDestroy(): void {
+      if (this.sub) {
+        this.sub.unsubscribe();
+      }
   }
 }
